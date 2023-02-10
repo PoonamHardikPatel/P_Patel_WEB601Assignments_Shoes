c7a127285eb2d83566cb5cfdb5f4b74780d31dc6
PoonamHardikPatel <105453380+PoonamHardikPatel@users.noreply.github.com>
2023-02-10 15:48:14 -0500

    Commit : 4 files modified

--- P_Patel_Shoes/src/app/content-list/content-list.component.ts
+++ P_Patel_Shoes/src/app/content-list/content-list.component.ts
@@ -1,26 +1,24 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
 import { ContentList } from '../helper-files/content-list';
+
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
-  styleUrls: ['./content-list.component.scss']
+  styleUrls: ['./content-list.component.css']
 })
-
-
-export class ContentListComponent {
-
-  contentArray: Content[];
-  constructor() {
-    this.contentArray = [{
+export class ContentListComponent implements OnInit {
+  Content: Content[] = [
+    {
       id: 1,
       title: 'mySampleTitle1',
       description: 'This is a description1',
       creator: 'Creator1',
       imgURL: 'https://justin-biber-babysong.com',
       type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2']
+      tags: ['Tag 1.1', 'Tag 1.2'],
     },
     {
       id: 2,
@@ -29,7 +27,7 @@ export class ContentListComponent {
       creator: 'Creator2',
       imgURL: 'https://justin-biber-babysong.com',
       type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2']
+      tags: ['Tag 1.1', 'Tag 1.2'],
     },
     {
       id: 3,
@@ -38,7 +36,7 @@ export class ContentListComponent {
       creator: 'Creator3',
       imgURL: 'https://justin-biber-babysong.com',
       type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2']
+      tags: ['Tag 1.1', 'Tag 1.2'],
     },
     {
       id: 4,
@@ -47,7 +45,7 @@ export class ContentListComponent {
       creator: 'Creator4',
       imgURL: 'https://justin-biber-babysong.com',
       type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2']
+      tags: ['Tag 1.1', 'Tag 1.2'],
     },
     {
       id: 5,
@@ -56,7 +54,7 @@ export class ContentListComponent {
       creator: 'Creator5',
       imgURL: 'https://justin-biber-babysong.com',
       type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2']
+      tags: ['Tag 1.1', 'Tag 1.2'],
     },
     {
       id: 6,
@@ -65,10 +63,93 @@ export class ContentListComponent {
       creator: 'Creator6',
       imgURL: 'https://justin-biber-babysong.com',
       type: 'rock music',
-      tags: ['Tag 1.1', 'Tag 1.2']
-    }
+      tags: ['Tag 1.1', 'Tag 1.2'],
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
+
+
+
+
+
+
+/*
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss'],
+})
+export class ContentListComponent {
+  contentArray: Content[];
+  constructor() {
+    this.contentArray = [
+      {
+        id: 1,
+        title: 'mySampleTitle1',
+        description: 'This is a description1',
+        creator: 'Creator1',
+        imgURL: 'https://justin-biber-babysong.com',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 2,
+        title: 'mySampleTitle2',
+        description: 'This is a description2',
+        creator: 'Creator2',
+        imgURL: 'https://justin-biber-babysong.com',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 3,
+        title: 'mySampleTitle3',
+        description: 'This is a description3',
+        creator: 'Creator3',
+        imgURL: 'https://justin-biber-babysong.com',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 4,
+        title: 'mySampleTitle4',
+        description: 'This is a description4',
+        creator: 'Creator4',
+        imgURL: 'https://justin-biber-babysong.com',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 5,
+        title: 'mySampleTitle5',
+        description: 'This is a description5',
+        creator: 'Creator5',
+        imgURL: 'https://justin-biber-babysong.com',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
+      {
+        id: 6,
+        title: 'mySampleTitle6',
+        description: 'This is a description6',
+        creator: 'Creator6',
+        imgURL: 'https://justin-biber-babysong.com',
+        type: 'rock music',
+        tags: ['Tag 1.1', 'Tag 1.2'],
+      },
     ];
   }
+  */
+
+
+
+
+
 
 
   /*
